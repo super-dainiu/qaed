690903b6bbba1d742c325139a4fd4c0c8985a642
Yanjun Shao <ys792@r602u03n01.grace.ycrc.yale.edu>
2024-04-08 03:25:34 -0400

    modify vec_err

--- test_relerr_fullrand.m
+++ test_relerr_fullrand.m
@@ -8,7 +8,7 @@ for n = order_n
     [P, D] = eigvec(Q, T);
     uni_err = rel_err(eye(n), Q' * Q);
     schur_err = rel_err(T, Q' * A * Q);
-    eigvec_err = rel_err(D, inv(P) * A * P);
+    eigvec_err = vec_err(D, inv(P) * A * P);
     fprintf('using aed, n = %d, uni_err = %.2e, schur_err = %.2e, eigvec_err = %.2e\n', n, uni_err, schur_err, eigvec_err);
 
     % load the matrix
@@ -16,10 +16,14 @@ for n = order_n
     [P, D] = eigvec(Q, T);
     uni_err = rel_err(eye(n), Q' * Q);
     schur_err = rel_err(T, Q' * A * Q);
-    eigvec_err = rel_err(D, inv(P) * A * P);
+    eigvec_err = vec_err(D, inv(P) * A * P);
     fprintf('using iqr, n = %d, uni_err = %.2e, schur_err = %.2e, eigvec_err = %.2e\n', n, uni_err, schur_err, eigvec_err);
 end
 
 function err = rel_err(A, B)
 err = norm(A - B, 'fro') / max(norm(A, 'fro'), norm(B, 'fro'));
+end
+
+function err = vec_err(A, P, D)
+err = norm(A * P - P * D, 'fro') / (norm(A, 'fro') + norm(D, 'fro')) * norm(P, 'fro');
 end
