5fcf888ffcd1b31b573aa56f5853735c1a5ab67b
Yanjun Shao <19307110036@fudan.edu.cn>
2024-05-09 06:58:40 -0400

    add

--- scripts/plot_examples.py
+++ scripts/plot_examples.py
@@ -30,8 +30,61 @@ def draw_upper_triangular_matrix_with_triangular_sylvester(n, square_size, step=
     plt.savefig(f'assets/sylvester_{n}_{step}.pdf', bbox_inches='tight')
     plt.close()
 
+def draw_sylvester_solve(n, square_size):
+    fig, ax = plt.subplots(figsize=(5, 5))
+    
+    for j in range(n-1):
+        for i in range(j, n-1):
+            square = plt.Rectangle((i, j), square_size, square_size, fc='#CCCCCC', ec='black')
+            ax.add_patch(square)
+    
+    ax.set_xlim(0, n-1)
+    ax.set_ylim(0, n-1)
+    ax.set_aspect('equal')
+    ax.invert_yaxis()
+    ax.axis('off')
+    
+    plt.tight_layout()
+    plt.savefig(f'assets/sylvester_solve_{n}_lhs.pdf', bbox_inches='tight')
+    plt.close()
+
+    fig, ax = plt.subplots(figsize=(5, 5))
+
+    for i in range(n-1):
+        square = plt.Rectangle((0, i), square_size, square_size, fc='blue', ec='black')
+        ax.add_patch(square)
+
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, n-1)
+    ax.set_aspect('equal')
+    ax.invert_yaxis()
+    ax.axis('off')
+
+    plt.tight_layout()
+    plt.savefig(f'assets/sylvester_solve_{n}_rhs.pdf', bbox_inches='tight')
+    plt.close()
+
+    fig, ax = plt.subplots(figsize=(5, 5))
+
+    square = plt.Rectangle((0, 0), square_size, square_size, fc='red', ec='black')
+    ax.add_patch(square)
+
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.set_aspect('equal')
+    ax.invert_yaxis()
+    ax.axis('off')
+
+    plt.tight_layout()
+    plt.savefig(f'assets/sylvester_solve_{n}_solution.pdf', bbox_inches='tight')
+    plt.close()
+    
+    
+
 # Usage
-n = 8
+n = 10
 square_size = 1
-for step in range(0, n+1):
-    draw_upper_triangular_matrix_with_triangular_sylvester(n, square_size, step)
+# for step in range(0, n+1):
+#     draw_upper_triangular_matrix_with_triangular_sylvester(n, square_size, step)
+
+draw_sylvester_solve(n, square_size)
